3208ddbb49e392a33d62e549e7778a52f8e9ca5f
sfny <sunrisefade+github@gmail.com>
2016-05-17 12:04:43 -0700

    template setup

---
+++ server/methods.js
@@ -0,0 +1,31 @@
+Meteor.methods({
+    addResolution(resolution) {
+        check(resolution, String);
+        if(!Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
+        Resolutions.insert({
+            text: resolution,
+            complete: false,
+            createdAt: new Date(),
+            user: Meteor.userId()
+        });     
+    },
+    toggleResolution(resolution) {
+        check(resolution, Object);
+
+        if(Meteor.userId() !== resolution.user) {
+            throw new Meteor.Error('not-authorized');
+        }
+        Resolutions.update(resolution._id, {
+            $set: {complete: !resolution.complete}
+        });
+    },
+    deleteResolution(resolution) {
+        check(resolution, Object);
+        if(Meteor.userId() !== resolution.user) {
+            throw new Meteor.Error('not-authorized');
+        }
+        Resolutions.remove(resolution._id);
+    }
+});
